1263145aacc2ff19391e56e37c0961e7d9af3290
Sejin Jeon <jinaidy93@gmail.com>
2019-04-28 14:31:46 +0900

    Initial commit

---
+++ src/main/java/com/sejin/springbootpractice/SpringbootPracticeApplication.java
@@ -0,0 +1,13 @@
+package com.sejin.springbootpractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootPracticeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootPracticeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sejin/springbootpractice/SpringbootPracticeApplicationTests.java
@@ -0,0 +1,16 @@
+package com.sejin.springbootpractice;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringbootPracticeApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
