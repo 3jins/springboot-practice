da6e38bdbd8a8f2c781bf342de2d87f4bd813305
Sejin Jeon <jinaidy93@gmail.com>
2019-08-05 00:43:24 +0900

    Connect root url to `/index.html`

---
+++ src/main/java/com/sejin/springbootpractice/WebConfiguration.java
@@ -0,0 +1,15 @@
+package com.sejin.springbootpractice;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfiguration implements WebMvcConfigurer {
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry
+            .addResourceHandler("/")
+            .addResourceLocations("/index.html");
+    }
+}
